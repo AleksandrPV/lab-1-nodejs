a9b3176715c13d25d1b07528fb6629073be9df15
Aleksandr Semenov <AVSemenov88@mail.ru>
2025-06-04 08:06:11 +0300

    feat: добавлен сервис MongoDB в docker-compose и обновлено подключение в main.js

--- main.js
+++ main.js
@@ -4,18 +4,18 @@ const userInfo = os.userInfo();
 const uid = userInfo.uid;
 const name = userInfo.username;
 const hostname = '0.0.0.0';
-const port = 3030;
+const port = process.env.PORT || 3000;
 const MongoClient = require("mongodb").MongoClient;
-const url = "mongodb://127.0.0.1:27017/";
+const url = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/nodejs";
 const mongoClient = new MongoClient(url);
-count = 0;
+let count = 0;
 
 async function run() {
     try {
         // Подключаемся к серверу
         await mongoClient.connect();
-        // обращаемся к базе данных пользователя
-        const db = mongoClient.db(`test1`);
+        // обращаемся к базе данных
+        const db = mongoClient.db();
         // выполняем пинг для проверки подключения
         const result = await db.command({ ping: 1 });
         console.log("Подключение с сервером успешно установлено");
@@ -27,13 +27,10 @@ async function run() {
     catch(err) {
         console.log("Возникла ошибка");
         console.log(err);
-    } finally {
-        // Закрываем подключение при завершении работы или при ошибке
-        await mongoClient.close();
-        console.log("Подключение закрыто");
     }
 }
 
+// Запускаем подключение к MongoDB
 run().catch(console.error);
 
 const server = http.createServer((req, res) => {
